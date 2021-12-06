4dda6ad7308d82711bfb5f3a044838052bd92735
Andriy Drobenyuk <drobenyuk.a@gmail.com>
2021-12-06 18:08:56 +0200

    Updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,10 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        presets: [
+                            '@babel/preset-env',
+                            ['@babel/preset-react', { runtime: 'automatic'}]
+                        ],
                         plugins: [
                             '@babel/plugin-transform-runtime',
                         ],
